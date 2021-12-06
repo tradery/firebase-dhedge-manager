38666eb661e70e327188b61ffb7d0cc9161b1738
AslanGoldenhour <aslan@tradery.io>
2021-12-06 17:50:26 -0400

    Package is stable with hello world function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
-const functions = require("firebase-functions");
+'use strict'
+/** EXPORT ALL FUNCTIONS
+ *
+ *   Loads all `.f.js` files
+ *   Exports a cloud function matching the file name
+ *
+ *   Based on this thread:
+ *     https://github.com/firebase/functions-samples/issues/170
+ */
+const glob = require('glob');
+const camelCase = require('camelcase');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const config = functions.config().firebase;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(config);
+
+const files = glob.sync('./**/*.f.js', { cwd: __dirname, ignore: './node_modules/**' });
+for (let f = 0, fl = files.length; f < fl; f++) {
+  const file = files[f];
+  const functionName = camelCase(file.slice(0, -5).split('/').join('_')); // Strip off '.f.js'
+  if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) {
+    exports[functionName] = require(file);
+  }
+}
---
+++ functions/public/hello/onRequest.f.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+
+/**
+ * Simple Hello World test
+ */
+exports = module.exports = functions
+    .runWith({
+        // Ensure the function has enough memory and time to process
+        timeoutSeconds: 540,
+        memory: "1GB",
+    })
+    .https
+    .onRequest(async (request, response) => {
+        cors(request, response, async () => {
+        // Perform desired operations...
+        functions.logger.info("Hello!", { structuredData: true });
+
+        // Send a response
+        response.status(200).send({ test: 'Testing functions' });
+
+        // Terminate the function
+        response.end();
+        })
+    });
