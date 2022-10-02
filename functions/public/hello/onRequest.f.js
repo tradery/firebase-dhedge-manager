2f55d2eb97ca51ba4bf3ce3b797924e7645535bd
AslanGoldenhour <aslan@tradery.io>
2022-10-02 22:43:45 +0300

    cleaned up sample routes and moved from config to secrets

--- functions/public/hello/onRequest.f.js
+++ functions/public/hello/onRequest.f.js
@@ -1,25 +1,21 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
+const helpers = require('./../../libs/helpers');
 
 /**
  * Simple Hello World test
  */
 exports = module.exports = functions
-    .runWith({
-        // Ensure the function has enough memory and time to process
-        timeoutSeconds: 120,
-        memory: "1GB",
-    })
     .https
     .onRequest(async (request, response) => {
         cors(request, response, async () => {
-        // Perform desired operations...
-        functions.logger.info("Hello!", { structuredData: true });
+            // Perform desired operations...
+            helpers.log("Hello!");
 
-        // Send a response
-        response.status(200).send({ test: 'Testing functions' });
+            // Send a response
+            response.status(200).send({ test: 'Testing functions' });
 
-        // Terminate the function
-        response.end();
+            // Terminate the function
+            response.end();
         })
     });
--- functions/public/helloAuth/onRequest.f.js
+++ functions/public/helloAuth/onRequest.f.js
@@ -1,15 +1,6 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
-const { DateTime } = require("luxon");
-
-function error(response, statusCode, message) {
-    response.status(statusCode).send({
-        "timestamp": DateTime.now(),
-        "status": statusCode,
-        "error": message
-    });
-    response.end();
-}
+const helpers = require('../../libs/helpers');
 
 /**
  * Authenticated Hello World test
@@ -19,24 +10,24 @@ exports = module.exports = functions
         // Ensure the function has enough memory and time to process
         timeoutSeconds: 120,
         memory: "1GB",
+        secrets: ["API_KEY"],
     })
     .https
     .onRequest(async (request, response) => {
         cors(request, response, async () => {
             try {
                 // Force POST
-                if (request.method !== "POST") return error(response, 400, 
+                if (request.method !== "POST") return helpers.error(response, 400, 
                     "Request method must be POST.");
             
                 // Handle Auth
                 const { authorization }  = request.headers;
-                if (!authorization) return error(response, 403, 
+                if (!authorization) return helpers.error(response, 403, 
                     "Unauthorized. A key must be provided in the `authorization` header.");
 
-                const config = functions.config();
-                functions.logger.info(functions.config().auth.key);
+                // helpers.log(process.env.API_KEY);
                 
-                if (authorization !== config.auth.key) return error(response, 403, 
+                if (authorization !== process.env.API_KEY) return helpers.error(response, 403, 
                     "Unauthorized. The API key provided is invalid.");
                 
                 // Authorized!
@@ -46,7 +37,7 @@ exports = module.exports = functions
                 response.end();
             } catch (err) {
                 functions.logger.error(err, { structuredData: true });
-                return error(response, 400, err.message);
+                return helpers.error(response, 400, err.message);
             }
         })
     });
