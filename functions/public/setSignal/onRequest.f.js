a97787df9efe292768704d77910a342563cab3e6
AslanGoldenhour <aslan@tradery.io>
2022-10-11 01:35:30 +0300

    Updated setSignal route to be more db efficient

--- functions/public/setSignal/onRequest.f.js
+++ functions/public/setSignal/onRequest.f.js
@@ -23,6 +23,12 @@ exports = module.exports = functions
     .onRequest(async (request, response) => {
         cors(request, response, async () => {
             try {
+                // Make sure our ENVs are set
+                if (helpers.getBasepath() === undefined || helpers.getBasepath() === '')
+                    throw new Error("LOCAL_BASEPATH &/OR PRODUCTION_BASEPATH is not defined on the server.");
+                if (process.env.API_KEY === undefined || process.env.API_KEY === '')
+                    throw new Error("API_KEY is not defined on the server.");
+
                 // Force POST
                 if (request.method !== "POST") return helpers.error(response, 400, 
                     "Request method must be POST.");
@@ -87,40 +93,47 @@ exports = module.exports = functions
                         shortToken = 'USDC';
                     }
 
-                // Save the new signal
-                await signalsRef.doc().set({
-                    long: longToken
-                    , short: shortToken
-                    , createdAt: FieldValue.serverTimestamp()
-                });
-                
-                helpers.log(
-                    portfolioDoc.data().fundName
-                     + ' has been instructed to long ' + longToken
-                     + ' and short ' + shortToken
-                );
+                // Get the value of the last saved signal
+                const signalsSnapshot = await signalsRef.orderBy('createdAt', 'desc').limit(1).get();
+                const lastSignal = helpers.snapshotToArray(signalsSnapshot)[0].data;
 
-                if (helpers.getBasepath() === undefined || helpers.getBasepath() === '')
-                    throw new Error("LOCAL_BASEPATH &/OR PRODUCTION_BASEPATH is not defined.");
+                // Check to see if our signals have changed
+                if (lastSignal.long === longToken && lastSignal.short === shortToken) {
+                    // Signals have not changed since the last time they were sent
+                    response.status(200).send({ message: 'Signal received. It has not changed.' });
+                } else {
+                    // Save the new signal
+                    await signalsRef.doc().set({
+                        long: longToken
+                        , short: shortToken
+                        , createdAt: FieldValue.serverTimestamp()
+                    });
+                    
+                    helpers.log(
+                        portfolioDoc.data().fundName
+                        + ' has been instructed to long ' + longToken
+                        + ' and short ' + shortToken
+                    );
 
-                // Rebalance the pool and debt in a new thread
-                // We're purposely not calling this with await
-                // because we don't want this script to hang. 
-                fetch(helpers.getBasepath()
-                    + 'publicRebalanceOnRequest',
-                    {
-                        method: 'POST',
-                        headers: { 
-                            'Content-Type': 'application/json',
-                            'Accept': 'application/json',
-                            'Accept-Encoding': 'deflate, gzip',
-                            'authorization': process.env.API_KEY
-                        },
-                        body: JSON.stringify({ 'secret': secret })
-                    }
-                );
-                
-                response.status(200).send({ message: 'Signal saved!' });
+                    // Rebalance the pool and debt in a new thread
+                    // We're purposely not calling this with await
+                    // because we don't want this script to hang. 
+                    fetch(helpers.getBasepath()
+                        + 'publicRebalanceOnRequest',
+                        {
+                            method: 'POST',
+                            headers: { 
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json',
+                                'Accept-Encoding': 'deflate, gzip',
+                                'authorization': process.env.API_KEY
+                            },
+                            body: JSON.stringify({ 'secret': secret })
+                        }
+                    );
+                    
+                    response.status(200).send({ message: 'New signal saved!' });
+                }
 
                 // Termininate the function
                 return response.end();
