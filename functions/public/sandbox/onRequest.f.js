8dd846f836f506a30d585be2bd5899ba4a636068
AslanGoldenhour <aslan@tradery.io>
2022-10-24 00:50:21 +0300

    testing cleanup

--- functions/public/sandbox/onRequest.f.js
+++ functions/public/sandbox/onRequest.f.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
+const { firestore } = require('firebase-admin');
 const cors = require('cors')({ origin: true });
-const _ = require('lodash');
+// const _ = require('lodash');
 const helpers = require('../../libs/helpers');
 const dhedge = require('../../libs/dhedge');
 
@@ -39,172 +40,54 @@ exports = module.exports = functions
                 
                 // Authorized!
 
-                // await btcSuperYield.long();
-                // const aaveBalances = await zapper.aaveBalances(process.env.POOL_ADDRESS);
-                // // helpers.log(aaveBalances);
-                // helpers.log(zapper.cleanAaveBalances(aaveBalances));
-
                 // Get the data from the request
-                // const { 
-                //     memberAddress
-                // } = request.body;
-                // await dhedge.addMember(memberAddress);
-                
+                const { secret } = request.body;
+
+                // Return an error if needed
+                if (secret === undefined || secret === '')
+                    throw new Error("A `secret` must be set.");
+
+                // Initialize Firebase components
+                const db = firestore();
+
+                // Get the doc for this portfolio
+                const portfoliosRef = db.collection('portfolios');
+                const portfolioRef = portfoliosRef.doc(secret);
+                const portfolioDoc = await portfolioRef.get();
+
+                // Get the transactions collection for logging
+                const txsRef = portfolioRef.collection('transactions');
+
+                // Make sure we have a valid portfolio
+                if (portfolioDoc.data() === undefined)
+                    throw new Error("Unknown `secret`");
+
+                // And that it's active
+                if (portfolioDoc.data().isActive === false)
+                    throw new Error("This secret is not active.");
+
+                // Yay! We're authorized!
+                const { poolContract, network } = portfolioDoc.data();
+
+                // SETUP A POOL
                 const pool = await dhedge.initPool(
                     process.env.MNEMONIC,
-                    process.env.POOL_ADDRESS,
-                    'polygon'
+                    poolContract,
+                    network
                 );
+
+                // Check wallet balances with dhedge
                 let tokens = await dhedge.getBalances(pool);
                 helpers.log(tokens);
 
-                /**
-                 * make sure debt is overpaid
-                 */
-                /**
-                 * @TODO Figure out why I'm being auto-liquidated
-                 */
-                // if (!_.isEmpty(tokens['aave']['variable-debt'])) {
-                //     tokens = await aave.reduceDebt(pool, tokens, 1.8);
-                //     helpers.log(tokens);
-                // }
-                
-                
-                // tokens = await dhedge.withdrawLentTokens(
-                //     pool, 
-                //     tokens, 
-                //     tokens['aave']['supply']['USDC'].address,
-                //     1000000,
-                // );
-                // helpers.log(tokens);
-                // // expect usdc in wallet
-
-                // tokens = await dhedge.tradeUniswap(
-                //     pool, 
-                //     tokens, 
-                //     tokens['wallet']['USDC'].address,
-                //     dhedge.symbolToAddress('WBTC'),
-                //     tokens['wallet']['USDC'].balanceInt,
-                // );
-                // helpers.log(tokens);
-                // // expect no usdc in wallet; some wbtc in wallet
-
-                // tokens = await dhedge.repayDebt(
-                //     pool, 
-                //     tokens, 
-                //     tokens['aave']['variable-debt']['WBTC'].address, 
-                //     tokens['aave']['variable-debt']['WBTC'].balanceInt,
-                // );
-                // helpers.log(tokens);
-                // // Expect no debt; some wbtc in wallet
-                
-                // tokens = await dhedge.withdrawLentTokens(
-                //     pool, 
-                //     tokens, 
-                //     tokens['aave']['supply']['USDC'].address,
-                //     tokens['aave']['supply']['USDC'].balanceInt,
-                // );
-                // helpers.log(tokens);
-                // // expect usdc in wallet
-
-
-                // tokens = await dhedge.lendDeposit(
-                //     pool, 
-                //     tokens, 
-                //     tokens['wallet']['WBTC'].address, 
-                //     tokens['wallet']['WBTC'].balanceInt,
-                // );
-                // helpers.log(tokens);
-                // // expect supply wbtc to go up; no wbtc in wallet
-
-                
-
-
-
-                // const tx1 = await dhedge.borrowDebt(pool, dhedge.tokens.polygon.USDC.address, '2000000');
-                // const tx2 = await dhedge.repayDebt(pool, dhedge.tokens.polygon.USDC.address, '5000000');
-                // // helpers.log('Borrowing 0.00005 WBTC');
-                // helpers.log(tx1);
-                // helpers.log(tx2);
-                
-                // console.log(dhedge.getBalance(poolInfo, dhedge.tokens.AAVEV3));
-                // console.log(await coinmarketcap.btcPrice());
-
-                // helpers.log('Trade WBTC to BTCBULL3X');
-                // const tx0 = await dhedge.trade(
-                //     dhedge.tokens.WBTC,
-                //     dhedge.tokens.BTCBULL3X,
-                //     dhedge.getBalance(poolInfo, dhedge.tokens.WBTC),
-                //     'TOROS'
-                // );
-                // helpers.log(tx0);
-
-                // helpers.log('Trade BTCBULL3X to WBTC');
-                // const tx0 = await dhedge.trade(
-                //     dhedge.tokens.BTCBULL3X,
-                //     dhedge.tokens.WBTC,
-                //     4400,
-                //     'TOROS'
-                // );
-                // helpers.log(tx0);
-
-                // helpers.log('Pausing for 3 seconds...');
-                // await delay(3000);
-
-                
-                // const tx1 = await dhedge.lendDeposit(
-                //     dhedge.tokens.USDC,
-                //     dhedge.getBalance(poolInfo, dhedge.tokens.USDC)
-                // );
-                // helpers.log('Lend USDC to AAVE');
-                // helpers.log(tx1);
-
-                // helpers.log('Pausing for 3 seconds...');
-                // await delay(3000);
-
-
-                // const tx2 = await dhedge.borrowDebt(
-                //     dhedge.tokens.WBTC,
-                //     await coinmarketcap.btcPrice
-                //     dhedge.getBalance(poolInfo, dhedge.tokens.WBTC)
-                // );
-                // helpers.log('Borrow USCD from AAVE');
-                // helpers.log(tx2);
-               
-                // const tx = await dhedge.lendDeposit(
-                //     '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', 
-                //     '0x09c5'
-                // );
-                // helpers.log(tx);
-
-                // const tx = await dhedge.repayDebt(
-                //     '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', 
-                //     '0x1bc182fa01307a40'
-                // );
-                // helpers.log(tx);
-
-
-
-                // const wbtcBalance = dhedge.getBalance(
-                //     poolInfo, 
-                //     dhedge.tokens.AAVEV3
-                // );
-                // console.log(wbtcBalance.toString());
-
-                // const tx = await dhedge.withdrawDeposit(
-                //     dhedge.tokens.WBTC, 
-                //     dhedge.getBalance(
-                //         poolInfo, 
-                //         dhedge.tokens.AAVEV3
-                //     ).div(wbtcBalance)
-                // );
-                // helpers.log('withdrawDeposit WBTC from AAVE3')
-                // helpers.log(wbtcBalance);
-                // helpers.log(tx);
-
-                // helpers.log('BTC: ' + await coinmarketcap.btcPrice());
-                // helpers.log('ETH: ' + await coinmarketcap.ethPrice());
-                // helpers.log('MATIC: ' + await coinmarketcap.maticPrice());
+                // Perform a trade
+                dhedge.lendDeposit(
+                    pool, 
+                    txsRef, 
+                    tokens, 
+                    tokens['wallet']['USDC'], 
+                    tokens['wallet']['USDC'].balanceInt
+                );
 
                 // Respond
                 response.status(200).send({ message: 'Success'});
