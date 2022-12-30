33a48b724b484913bbc6cc1fdd63b13472b7171f
AslanGoldenhour <aslan@tradery.io>
2022-12-30 08:04:04 +0200

    changed rebalancing to every 6 hours

--- functions/scheduled/rebalanceDebt/onRun.f.js
+++ functions/scheduled/rebalanceDebt/onRun.f.js
@@ -21,7 +21,7 @@ const helpers = require('../../libs/helpers');
         ],
     })
     .pubsub
-    .schedule('every 5 minutes')
+    .schedule('every 6 hours')
     .onRun(async (context) => {
         try {
             // Make sure our ENVs are set
